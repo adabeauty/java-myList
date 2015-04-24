5226ef4afd47f76c55c959ff09116d7888ccc1d2
hawang <654296217@qq.com>
2015-04-24 17:42:38 +0800

    add main.java setting.gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import com.thoughtworks.gradle.MyList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyList<String> myList = new MyList<String>();
+
+        myList.add("hello");
+        myList.add(", ");
+        myList.add("world");
+        myList.add("!");
+
+
+        String string = "";
+        for(int i=0; i<myList.size(); i++){
+            string += myList.get(i);
+        }
+        System.out.println("The sentence has "  + myList.size() + " elements. It is \"" + string +"\".");
+    }
+}
